9cfa954b08c5a50abf97fd17ed23240c52aa5993
Nekekys <45885162+Nekekys@users.noreply.github.com>
2020-11-09 21:50:48 +0400

    Add files via upload

---
+++ precache-manifest.9c592c58b1a2bdfef5d5ba26646067ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281407ef957920008143fe1e3f1261fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b44f8b8b99639770ead1",
+    "url": "/static/css/main.1f657c06.chunk.css"
+  },
+  {
+    "revision": "b3fdc54fea27974af928",
+    "url": "/static/js/2.fcade933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcade933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44f8b8b99639770ead1",
+    "url": "/static/js/main.51cc2aed.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
